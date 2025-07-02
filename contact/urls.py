78ed44f3d0a15116b8fa2a063bfe060c6c9723f5
Álvaro Kayc <kayycsantos@gmail.com>
2025-07-02 14:48:03 -0300

    requirements

--- contact/urls.py
+++ contact/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('contact/create/', views.create, name='create'),
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
     
-     # User
+    # User
     path('user/create/', views.register, name='register'),
     path('user/login/', views.login_view, name='login'),
     path('user/logout/', views.logout_view, name='logout'),
